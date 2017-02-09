9e6f2605d295900f6ff71e342ca311536c824ca9
lulei11 <lulei@jd.com>
2017-02-09 16:30:23 +0800

    test

--- src/main/resources/lua/multiget.lua
+++ src/main/resources/lua/multiget.lua
@@ -9,8 +9,8 @@ local parser = require "redis.parser"
  table.insert(cmdlist,cmd1)
  table.insert(cmdlist,cmd2)
 
- local reqs = {}
- for i,req in ipairs(cmdlist) do 
+  local reqs = {}
+  for i,req in ipairs(cmdlist) do 
  	local raw_reqs = {}
         for j, req2 in ipairs(req) do
 		table.insert(raw_reqs,parser.build_query(req2))
@@ -18,17 +18,16 @@ local parser = require "redis.parser"
 	ngx.say(req[1][2].." route ".. route.getLocation(req[1][2]));
  	table.insert(reqs,{"/"..route.getLocation(req[1][2]).."?"..#req,{
 		body=table.concat(raw_reqs,"")}});
- end
+  end
  
- local resps = { ngx.location.capture_multi(reqs) }
+  local resps = { ngx.location.capture_multi(reqs) }
  
- for i, resp in ipairs(resps) do
+  for i, resp in ipairs(resps) do
  	if resp.status == 200 then
 		local replies = parser.parse_replies(resp.body,#cmdlist[i])
 		for j,reply in ipairs(replies) do
 			ngx.say(reply[1])
 		end
 	end
- end
-
+  end
 
--- src/main/resources/lua/multiget2.lua
+++ src/main/resources/lua/multiget2.lua
@@ -9,26 +9,36 @@ local parser = require "redis.parser"
  table.insert(cmdlist,cmd1)
  table.insert(cmdlist,cmd2)
 
- local reqs = {}
- for i,req in ipairs(cmdlist) do 
+ local keylist = {}
+ table.insert(keylist,"name1")
+ table.insert(keylist,"name2")
+
+local function multiget(keylist,cmdlist) 
+  local reqs = {}
+  for i,req in ipairs(cmdlist) do 
  	local raw_reqs = {}
         for j, req2 in ipairs(req) do
 		table.insert(raw_reqs,parser.build_query(req2))
 	end
-	ngx.say(req[1][2].." route ".. route.getLocation(req[1][2]));
+	--ngx.say(req[1][2].." route ".. route.getLocation(req[1][2]));
  	table.insert(reqs,{"/"..route.getLocation(req[1][2]).."?"..#req,{
 		body=table.concat(raw_reqs,"")}});
- end
+  end
  
- local resps = { ngx.location.capture_multi(reqs) }
+  local resps = { ngx.location.capture_multi(reqs) }
  
- for i, resp in ipairs(resps) do
- 	if resp.status == 200 then
+  for i, resp in ipairs(resps) do
+ 	if resp.status == 200 and resp.body then
 		local replies = parser.parse_replies(resp.body,#cmdlist[i])
 		for j,reply in ipairs(replies) do
-			ngx.say(reply[1])
+			ngx.say(keylist[i].."="..reply[1])
 		end
+	else
+		ngx.say("not found")
 	end
- end
+  end
+end
 
+
+multiget(keylist,cmdlist);
 
